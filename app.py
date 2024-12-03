98a37672f9fe4f562d693f4cd637a163f538abdf
Rithvik Manda <m.rithvik2710@gmail.com>
2024-12-03 16:20:39 +0530

    Config

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import streamlit as st
 from groq import Groq
 
 
-df1=pd.read_csv("datasets/AAPL_monthly_data.csv") #path to dataset1
-df2=pd.read_csv("datasets/AMZN_monthly_data.csv") #path to dataset2
-df3=pd.read_csv("datasets/AVGO_monthly_data.csv") #path to dataset3
-df4=pd.read_csv("datasets/META_monthly_data.csv") #path to dataset4
-df5=pd.read_csv("datasets/MSFT_monthly_data.csv") #path to dataset5
+df1=pd.read_csv("dataset/amazon_inv.csv") #path to dataset1
+df2=pd.read_csv("C:/Users/HP/OneDrive/Desktop/finance/dataset/apple_inv.csv") #path to dataset2
+df3=pd.read_csv("dataset/meta_inv.csv") #path to dataset3
+df4=pd.read_csv("dataset/ndx_inv.csv") #path to dataset4
+df5=pd.read_csv("dataset/nvidia_inv.csv") #path to dataset5
 
 df1_sample = df1.to_string(index=False)
 df2_sample = df2.to_string(index=False)
@@ -33,38 +33,30 @@ config_data = json.load(open(f"{working_dir}/config.json"))
 
 GROQ_API_KEY = config_data["GROQ_API_KEY"]
 
-# save the api key to environment variable
 os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 
 client = Groq()
-
-# initialize the chat history as streamlit session state of not present already
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
-# streamlit page title
-st.title("🦙 Finance ChatBot")
 
-# display chat history
+st.title("Risky Bot")
+
 for message in st.session_state.chat_history:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-
-# input field for user's message:
-user_prompt = st.chat_input("Ask LLAMA...")
+user_prompt = st.chat_input("Ask Risky Bot...")
 
 if user_prompt:
-
     st.chat_message("user").markdown(user_prompt)
     st.session_state.chat_history.append({"role": "user", "content": user_prompt})
 
-    # sens user's message to the LLM and get a response
     messages = [
         {"role": "system", "content": f"""
             You are chatbot and you must greet the user they greet you and You are a highly skilled Finance Assistant AI specializing in stock performance measurement and risk analytics. Your primary tasks include fetching historical stock and benchmark data using APIs like Yahoo Finance, calculating monthly, cumulative, and annualized returns over various periods (1M, 3M, 6M, 1Y, 2Y, 3Y, 5Y), and computing risk statistics like Sharpe Ratios to identify the best-performing stock by return per unit risk. You generate clear, actionable insights, create visualizations (e.g., rebased stock price charts), and guide users in building technical solutions, such as integrating APIs into web interfaces for data retrieval. Always provide precise, concise, and user-friendly explanations for calculations, insights, or code implementations, and proactively ask clarifying questions when user inputs are incomplete.
             {df}
-            Calculate monthly, cumulative, and annualized returns over various periods (1M, 3M, 6M, 1Y, 2Y, 3Y, 5Y), and computing risk statistics like Sharpe Ratios to identify the best-performing stock by return per unit risk based on this data. Please provide the results.  Don't show me the code that you have used this is a strict warning only show the results for the user input. When you display the output display it properly with propoer identation.   """},
+            Calculate 5 years Anual sharp ratio , anual returns , risk free rate, anualized standard deviation """},
         *st.session_state.chat_history
     ]
 
@@ -76,6 +68,5 @@ if user_prompt:
     assistant_response = response.choices[0].message.content
     st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
 
-    # display the LLM's response
     with st.chat_message("assistant"):
         st.markdown(assistant_response)
